a7caf6c0ce0bcf26854d7cddfcebbede035a62b2
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-03-11 12:34:14 +0700

    menambahkan prosedur

--- TugasConditionalStatement.cpp
+++ TugasConditionalStatement.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+void inputData(float &berat, float &tinggi) { //PROSEDUR
+    cout << "Masukkan berat badan (KG) =";
+        cin >> berat;
+    
+    cout << "Masukkan tinggi badan (Meter) = ";
+        cin >> tinggi;
+}
+
